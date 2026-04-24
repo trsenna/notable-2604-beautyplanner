38f139c41d1d5b4a39da91abcd94a7209c8cacec
Thiago Senna <trsenna@gmail.com>
2026-04-24 00:36:19 +0000

    Add initial planner-query service

---
+++ services/planner-query/src/test/java/io/planner/query/PlannerQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.planner.query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
