6d77b115a41fc23333709a6ab1141ce4f7cc568a
Thiago Senna <trsenna@gmail.com>
2026-04-24 00:39:11 +0000

    Add initial planner-sync service

---
+++ services/planner-sync/src/main/java/io/planner/sync/PlannerSyncApplication.java
@@ -0,0 +1,13 @@
+package io.planner.sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerSyncApplication.class, args);
+	}
+
+}
---
+++ services/planner-sync/src/test/java/io/planner/sync/PlannerSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package io.planner.sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
